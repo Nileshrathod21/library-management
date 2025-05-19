fd32fb326f8bb3ad9fc824894b285c5be83d67d8
Nileshrathod21 <im.nileshrathod@gmail.com>
2025-05-19 19:02:40 +0530

    user data update

--- src/Components/UserData.js
+++ src/Components/UserData.js
@@ -1,8 +1,8 @@
 export const UserData = [
-  { id: 1, name: "Alice", grade: "A", enrolled: true },
-  { id: 2, name: "Bob", grade: "B", enrolled: false },
-  { id: 3, name: "Alice", grade: "A", enrolled: true },
-  { id: 4, name: "Bob", grade: "B", enrolled: false },
-  { id: 5, name: "Alice", grade: "A", enrolled: true },
-  { id: 6, name: "Bob", grade: "B", enrolled: false },
+  { id: 1, name: "Bhadresh", grade: "A", enrolled: true },
+  { id: 2, name: "Vishal", grade: "B", enrolled: false },
+  { id: 3, name: "Chirag", grade: "A", enrolled: true },
+  { id: 4, name: "Brijesh", grade: "B", enrolled: false },
+  { id: 5, name: "Lucky", grade: "A", enrolled: true },
+  { id: 6, name: "chanden", grade: "B", enrolled: false },
 ];
